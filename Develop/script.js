11b68e675e923e737f3b3fc08d561c752ff5e6ff
robbiemurphy <rmurphy987@gmail.com>
2022-01-23 22:48:05 -0600

    css stylings linked to timeblocks in html / local storage written in js

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,22 @@
 // TEST LINK
 console.log("Hello");
 
+// RESPONSIBLE FOR CURRENT DATE DISPLAYED AT TOP
 var currentDay = moment().format("dddd, MMM Do YYYY");
+$("#currentDay").html(currentDay);
+
+//$(document).ready(function () {
+//    localStorage.setItem(text, time);
+//}
+
+
+// LOCAL STORAGE
+$("#8am .description").val(localStorage.getItem("8am"));
+$("#9am .description").val(localStorage.getItem("9am"));
+$("#10am .description").val(localStorage.getItem("10am"));
+$("#11am .description").val(localStorage.getItem("11am"));
+$("#12pm .description").val(localStorage.getItem("12pm"));
+$("#1pm .description").val(localStorage.getItem("1pm"));
+$("#2pm .description").val(localStorage.getItem("2pm"));
+$("#3pm .description").val(localStorage.getItem("3pm"));
+$("#4pm .description").val(localStorage.getItem("4pm"));
